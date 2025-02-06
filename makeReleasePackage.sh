1fe0cf1bf1a9ad26eabbf6b7e3281a61aca974f8
greenwave-1 <150715874+greenwave-1@users.noreply.github.com>
2025-02-06 14:18:15 -0500

    Meta changes Added script for creating releases Changed meta.xml to match those changes Added a temporary icon.png

---
+++ makeReleasePackage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# this script will make a zip for uploading to github as a release.
+# this script expects to be ran from the root directory of the project,
+# and expects 1 argument, a version number
+
+if [ $# -ne 1 ]
+then
+  echo "Provide a version number and nothing else"
+  exit 1
+fi
+
+FILENAME=`basename "$PWD"`
+DATE=`date -I`
+
+make all
+
+mkdir -p release/wii/apps/FossScope
+cp wii-homebrew-channel-data/meta.xml release/wii/apps/FossScope/
+cp wii-homebrew-channel-data/icon.png release/wii/apps/FossScope/
+
+mv ${FILENAME}_wii.dol release/wii/apps/FossScope/boot.dol
+mv ${FILENAME}_gc.dol release/FossScope_GC.dol
+
+sed -i 's/VERSION/'$1'/g' release/wii/apps/FossScope/meta.xml
+sed -i 's/DATE/'$DATE'/g' release/wii/apps/FossScope/meta.xml
+
+make clean
+
+cd release/wii/
+zip ../wii.zip apps/FossScope/*
