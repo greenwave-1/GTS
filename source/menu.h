09c36fd057ff9d84a603dfb51bea94953aa89610
greenwave-1 <150715874+greenwave-1@users.noreply.github.com>
2025-03-17 15:50:34 -0400

    Revert "Dirty implementation of reaction test"
    This reverts commit 3553a5a7ba86900f0c54100bad6389c294edeb04.

--- source/menu.h
+++ source/menu.h
@@ -8,9 +8,7 @@
 #include <stdbool.h>
 
 // enum for keeping track of the currently displayed menu
-enum CURRENT_MENU { MAIN_MENU, CONTROLLER_TEST, WAVEFORM, PLOT_2D,
-		IMAGE_TEST, FILE_EXPORT, WAITING_MEASURE, COORD_MAP,
-		REACTION_TEST_PRE, REACTION_TEST, REACTION_TEST_INPUT, REACTION_TEST_POST, ERR };
+enum CURRENT_MENU { MAIN_MENU, CONTROLLER_TEST, WAVEFORM, PLOT_2D, IMAGE_TEST, FILE_EXPORT, WAITING_MEASURE, COORD_MAP, ERR };
 
 // functions for drawing the individual menus
 bool menu_runMenu(void *currXfb);
@@ -21,9 +19,6 @@ void menu_2dPlot(void *currXfb);
 void menu_fileExport(void *currXfb);
 void menu_waitingMeasure(void *currXfb);
 void menu_coordinateViewer(void *currXfb);
-void menu_reactionTest(void *currXfb);
-void menu_reactionTestPre(void *currXfb);
-void menu_reactionTestInput(void *currXfb);
-void menu_reactionTestPost(void *currXfb);
+
 
 #endif //FOSSSCOPE_MENU_H
