ab7f6f85dde9f3342f3a01720a767ff81240e4e1
greenwave-1 <150715874+greenwave-1@users.noreply.github.com>
2025-07-15 15:59:21 -0400

    Gate Measure Add proper instructions Add stick toggle Add reset button

--- source/submenu/gate.c
+++ source/submenu/gate.c
@@ -16,6 +16,8 @@ static u32 *pressed = NULL;
 static u32 *held = NULL;
 static bool buttonLock = false;
 static u8 buttonPressCooldown = 0;
+static u8 yPressFrameCounter = 0;
+static bool yHeldAfterReset = false;
 
 static sampling_callback cb;
 static u64 prevSampleCallbackTick = 0;
@@ -27,8 +29,11 @@ typedef struct GateMinMax {
 	bool init;
 	int8_t min;
 	int8_t max;
+	int8_t cMin;
+	int8_t cMax;
 } GateMinMax;
 static GateMinMax gateMinMax[256] = { 0 };
+static bool showC = false;
 
 void gateSamplingCallback() {
 	// time from last call of this function calculation
@@ -56,16 +61,29 @@ void gateSamplingCallback() {
 	
 	if (menuState == GATE_POST_SETUP && state == GATE_POST_INIT) {
 		int currX = PAD_StickX(0), currY = PAD_StickY(0);
+		int currCX = PAD_SubStickX(0), currCY = PAD_SubStickY(0);
 		
-		if (!gateMinMax[currX + 128].init) {
-			gateMinMax[currX + 128].init = true;
-		}
-		
-		if (gateMinMax[currX + 128].min > currY) {
-			gateMinMax[currX + 128].min = currY;
-		}
-		if (gateMinMax[currX + 128].max < currY) {
-			gateMinMax[currX + 128].max = currY;
+		if (showC) {
+			if (!gateMinMax[currCX + 128].init) {
+				gateMinMax[currCX + 128].init = true;
+			}
+			if (gateMinMax[currCX + 128].cMin > currCY) {
+				gateMinMax[currCX + 128].cMin = currCY;
+			}
+			if (gateMinMax[currCX + 128].cMax < currCY) {
+				gateMinMax[currCX + 128].cMax = currCY;
+			}
+		} else {
+			if (!gateMinMax[currX + 128].init) {
+				gateMinMax[currX + 128].init = true;
+			}
+			
+			if (gateMinMax[currX + 128].min > currY) {
+				gateMinMax[currX + 128].min = currY;
+			}
+			if (gateMinMax[currX + 128].max < currY) {
+				gateMinMax[currX + 128].max = currY;
+			}
 		}
 	}
 	
@@ -81,7 +99,14 @@ static void setup(u32 *p, u32 *h) {
 }
 
 static void displayInstructions(void *currXfb) {
-	printStr("doin the stuff", currXfb);
+	setCursorPos(2, 0);
+	printStr("Move the stick around the gate to measure.\n"
+			 "Press X to toggle which stick gate is measured.\n"
+			 "Hold Y to reset current measure.\n"
+			 "Measure will also be reset after returning to Main Menu.", currXfb);
+	
+	setCursorPos(21, 0);
+	printStr("Press Z to close instructions.", currXfb);
 	
 	if (!buttonLock) {
 		if (*pressed & PAD_TRIGGER_Z) {
@@ -105,19 +130,50 @@ void menu_gateMeasure(void *currXfb, u32 *p, u32 *h) {
 						gateMinMax[i].init = false;
 						gateMinMax[i].min = 0;
 						gateMinMax[i].max = 0;
+						gateMinMax[i].cMin = 0;
+						gateMinMax[i].cMax = 0;
 					}
+					showC = false;
+					yPressFrameCounter = 0;
 					state = GATE_POST_INIT;
 				case GATE_POST_INIT:
+					setCursorPos(2, 0);
+					printStr("Press Z for Instructions", currXfb);
+					setCursorPos(21,0);
+					printStr("Current stick: ", currXfb);
+					if (showC) {
+						printStr("C-Stick", currXfb);
+					} else {
+						printStr("Analog Stick", currXfb);
+					}
+					if (yPressFrameCounter != 0) {
+						setCursorPos(20, 0);
+						printStr("Resetting", currXfb);
+						int remainder = yPressFrameCounter / 30;
+						while (remainder != 0) {
+							printStr(".", currXfb);
+							remainder--;
+						}
+					}
 					// draw box around plot area
 					DrawBox(SCREEN_POS_CENTER_X - 128, SCREEN_POS_CENTER_Y - 128,
 					        SCREEN_POS_CENTER_X + 128, SCREEN_POS_CENTER_Y + 128,
 					        COLOR_WHITE, currXfb);
 					
 					// draw each
-					for (int i = 0; i < 256; i++) {
-						if (gateMinMax[i].init) {
-							DrawDot(SCREEN_POS_CENTER_X - 128 + i, SCREEN_POS_CENTER_Y - gateMinMax[i].max, COLOR_WHITE, currXfb);
-							DrawDot(SCREEN_POS_CENTER_X - 128 + i, SCREEN_POS_CENTER_Y - gateMinMax[i].min, COLOR_WHITE, currXfb);
+					if (showC) {
+						for (int i = 0; i < 256; i++) {
+							if (gateMinMax[i].init) {
+								DrawDot(SCREEN_POS_CENTER_X - 128 + i, SCREEN_POS_CENTER_Y - gateMinMax[i].cMax, COLOR_YELLOW, currXfb);
+								DrawDot(SCREEN_POS_CENTER_X - 128 + i, SCREEN_POS_CENTER_Y - gateMinMax[i].cMin, COLOR_YELLOW, currXfb);
+							}
+						}
+					} else {
+						for (int i = 0; i < 256; i++) {
+							if (gateMinMax[i].init) {
+								DrawDot(SCREEN_POS_CENTER_X - 128 + i, SCREEN_POS_CENTER_Y - gateMinMax[i].max, COLOR_WHITE, currXfb);
+								DrawDot(SCREEN_POS_CENTER_X - 128 + i, SCREEN_POS_CENTER_Y - gateMinMax[i].min, COLOR_WHITE, currXfb);
+							}
 						}
 					}
 					
@@ -127,6 +183,23 @@ void menu_gateMeasure(void *currXfb, u32 *p, u32 *h) {
 							buttonLock = true;
 							buttonPressCooldown = 5;
 						}
+						if (*pressed & PAD_BUTTON_X) {
+							showC = !showC;
+							buttonLock = true;
+							buttonPressCooldown = 5;
+						}
+					}
+					if (*held & PAD_BUTTON_Y) {
+						if (!yHeldAfterReset) {
+							yPressFrameCounter++;
+							if (yPressFrameCounter == 91) {
+								state = GATE_INIT;
+								yHeldAfterReset = true;
+							}
+						}
+					} else {
+						yPressFrameCounter = 0;
+						yHeldAfterReset = false;
 					}
 					break;
 			}
